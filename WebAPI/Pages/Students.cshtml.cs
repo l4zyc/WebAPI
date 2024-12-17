a8371bef511ae68e12b9d08bcaf52d1dddad7bba
l4zyC <appleyoukick@gmail.com>
2024-12-17 07:50:30 +0700

    Commit Init

--- WebAPI/Pages/Students.cshtml.cs
+++ WebAPI/Pages/Students.cshtml.cs
@@ -1,15 +1,17 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAPI.Pages.Student
 {
     public class StudentModel : PageModel 
     {
         private readonly AppDBContext _context;
+        private readonly ILogger _logger;
 
-        public StudentModel(AppDBContext context) {
+        public StudentModel(AppDBContext context, ILogger<StudentModel> logger) {
             _context = context;
+            _logger = logger;
         }
 
         public IActionResult OnGet()
@@ -19,18 +21,54 @@ namespace WebAPI.Pages.Student
 
          //to bind the properties (the getters and setters) of the PageModel to incoming request data
         [BindProperty]
-        public WebAPI.Models.Student? student {get; set;}
+        public WebAPI.Models.Request.StudentRequest? request {get; set;}
+        [TempData]
+        public String StatusMessage { get; set; } = String.Empty;
 
         public async Task<IActionResult> OnPostAsync() {
             // if the form data sent by the user is valid
+            ModelState.Remove("request.StudentID"); //Remove StudentID from Required Field
+
+            String studentID;
+            var studentData = await _context.Student.OrderByDescending(x => x.StudentID).FirstOrDefaultAsync();
+
+            if(studentData == null) {
+                studentID = "ST001";
+                return Page();
+            }
+
+            String latStudentID = studentData.StudentID.Substring(2);
+            Int32 incr = Int32.Parse(latStudentID) + 1;
+            studentID = $"ST{incr:D3}";
+
+            var student = new Models.Student {
+                StudentID = studentID,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Age = request.Age,
+                MajorID = request.MajorID
+            };
+
+
             if(!ModelState.IsValid) {
+                StatusMessage = "Error";
+                _logger.LogWarning("ModelState is invalid.");
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    _logger.LogWarning($"ModelState error: {error.ErrorMessage}");
+                }
                 return Page();
             }
 
             if(student != null) _context.Student.Add(student);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./index");
+            StatusMessage = "Success";
+
+            request = null;
+            ModelState.Clear();
+
+            return Page();
         }
     }
 }
--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95667eaeafd69238a8cfe42356404c65cae71024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e984ebc76372e4afcb0ae92c95601169a76d69a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
